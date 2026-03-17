f86e700072e5de9d6d465e4b99580c15f611a45a
Natan Garcia Rodrigues <art0066386@aluno.firjansenaisesi.com.br>
2026-03-17 16:25:01 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const sequelize = require('./database/db');
+const cors = require('cors');
+const livroControllers = require('./controllers/LivroControllers');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/livros', livroControllers);
+
+async function startServer() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com MySQL estabelecido.');
+        await sequelize.sync(); // cria a tabela lá no banco de dados se não existir
+
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Erro ao iniciar o servidor: ', error);
+    }
+}
+
+startServer();
